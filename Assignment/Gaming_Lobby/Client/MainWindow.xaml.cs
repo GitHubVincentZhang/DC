3ca10dbfc577b0ab511dc85ecec1dd13ed86c276
Hanbo Zhang <vincentzhang515@gmail.com>
2024-09-12 14:39:24 +0800

    Commit

--- Assignment/Gaming_Lobby/Client/MainWindow.xaml.cs
+++ Assignment/Gaming_Lobby/Client/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Client.;
+using Client;
 
 
 namespace Client
